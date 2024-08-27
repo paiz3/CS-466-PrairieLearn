e0ced0bc3f7e7eef47ec83f6558da965684f46e7
Nico Ritschel <ritschel@illinois.edu>
2024-08-27 14:39:53 +0000

    Pre-Course-Background: modify server.py

--- questions/Survey-Questions/Pre-Course-Background/server.py
+++ questions/Survey-Questions/Pre-Course-Background/server.py
@@ -18,9 +18,3 @@ def grade(data):
     data['partial_scores']['q1'] = {'score': None}
     data['partial_scores']['q2'] = {'score': None}
     data['partial_scores']['q3'] = {'score': None}
-    data['partial_scores']['q4'] = {'score': None}
-    data['partial_scores']['q5'] = {'score': None}
-    data['partial_scores']['q6'] = {'score': None}
-    data['partial_scores']['q7'] = {'score': None}
-    data['partial_scores']['q8'] = {'score': None}
-    data['partial_scores']['q9'] = {'score': None}
